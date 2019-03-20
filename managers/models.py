a0f92eeaddf5d4f604f1aaecf3be4a8e6f4fa364
Piter Pentester <user@localhost.localdomain>
2019-03-20 11:13:25 -0400

    templates, models

--- managers/models.py
+++ managers/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=32)
+    middle_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    phone = models.CharField(max_length=10)
+    mail = models.CharField(max_length=32)
+    
+class Type_Contact(models.Model):
+    name = models.CharField(max_length=36)
+class Type_Advetising(models.Model):
+    name = models.CharField(max_length=32)
+
 class Manager(models.Model):
     first_name = models.CharField(max_length=32)
     middle_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     phone = models.CharField(max_length=10)
+class Order(models.Model):
+    id_quartal = models.IntegerField()
+    section = models.IntegerField()
+    floor = models.IntegerField()
+    flat = models.IntegerField()
+    userid = models.IntegerField()
